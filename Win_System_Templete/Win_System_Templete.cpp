e161b3b525e865abd7090e1102c52fba1ddd163e
RainMaker3363 <jsc22637@gmail.com>
2017-10-16 21:24:18 +0900

    # Sync 5
    - 파이프 라인을 통한 양방향, 단방향 통신을 공부
    - 스케쥴링 알고르짐을 학습 (선점, 비선점)
    - Win_System_Templete에 프로세스 목록 출력 및 종료 기능 추가

--- Win_System_Templete/Win_System_Templete.cpp
+++ Win_System_Templete/Win_System_Templete.cpp
@@ -4,10 +4,14 @@
 #include "stdafx.h"
 #include <locale.h>
 #include <Windows.h>
+#include <TlHelp32.h>
 
 
 #define STR_LEN 256
 #define CMD_TOKEN_NUM 10
+
+#define MAX_STRING_NUM 100
+#define MAX_STRING_LEN 256
 
 TCHAR ERROR_CMD[]
 = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
@@ -24,6 +28,8 @@ int tmain(int argc, TCHAR* argv[])
 {
 	// 한글 입력이 가능하게 하기 위해...
 	_tsetlocale(LC_ALL, _T("Korean"));
+
+	SortString();
 
 	// 매개변수 전달인자가 있는 경우 처리를 위한 것이다.
 	// Start 명령어를 위한 처리로 생각하면 좋겠다.
@@ -87,6 +93,149 @@ int CmdReadTokenSize(void)
 }
 
 //==========================================================================
+// 프로세스들의 정보들을 출력하기 위한 함수
+// 스냅샷 기능을 이용하여 현재 프로세스의 정보와 그 뒤의 프로세스의 정보들을
+// 이름과 ID를 출력시킨다.
+//==========================================================================
+void ListProcessInfo(void)
+{
+	// 현재 프로세스의 정보를 캡쳐한다.
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolHeal32Sanpshot error! \n"));
+		return;
+	}
+
+	// 프로세스의 정보를 얻기 위한 구조체 변수
+	PROCESSENTRY32 pe32;
+
+	// 구조체를 초기화한다.
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// 첫번째 프로세스 정보 얻음
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+
+	do
+	{
+		// 프로세스 이름, ID 정보 출력
+		_tprintf(_T("%25s %5d \n"), pe32.szExeFile, pe32.th32ProcessID);
+
+	} while (Process32Next(hProcessSnap, &pe32));
+	
+	CloseHandle(hProcessSnap);
+}
+
+//==========================================================================
+// 프로세스를 종료 시키는 함수
+//==========================================================================
+void KillProcess(void)
+{
+	// 현재 프로세스의 정보를 캡쳐한다.
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolHeal32Sanpshot error! \n"));
+		return;
+	}
+
+	// 프로세스의 정보를 얻기 위한 구조체 변수
+	PROCESSENTRY32 pe32;
+
+	// 구조체를 초기화한다.
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// 첫번째 프로세스 정보 얻음
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+	
+	HANDLE hProcess;
+	BOOL isKill = FALSE;
+
+	do
+	{
+		// 입력된 프로세스 이름과 비교한다.
+		if (_tcscmp(pe32.szExeFile, cmdTokenList[1]) == 0)
+		{
+			// 프로세스 ID를 핸들로 반환하는 과정
+			hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+
+			if (hProcess != NULL)
+			{
+				// 프로세스를 강제로 종료시킨다.
+				TerminateProcess(hProcess, -1);
+				isKill = TRUE;
+			}
+
+			CloseHandle(hProcess);
+			break;
+		}
+	} while (Process32Next(hProcessSnap, &pe32));
+	
+	CloseHandle(hProcessSnap);
+
+	if (isKill == FALSE)
+	{
+		_tprintf(_T("Kill Process Fail, Try Agian! \n"));
+	}
+}
+
+//==========================================================================
+// 콘솔로 부터 문자열을 읽은 후 리다이렉션을 시키는 함수
+//==========================================================================
+void SortString(void)
+{
+	TCHAR stringArr[MAX_STRING_NUM][MAX_STRING_LEN];
+
+	// 콘솔로부터 문자열을 읽어 들인다.
+	int nStr;
+
+	for (nStr = 0; nStr < MAX_STRING_NUM; nStr++)
+	{
+		TCHAR* isEOF = _fgetts(stringArr[nStr], MAX_STRING_LEN, stdin);
+
+		// EOF는 Ctrl + z
+		if (isEOF == NULL)
+		{
+			break;
+		}
+	}
+
+	// String Bubble Sort
+	// 성능 고려하지 않고 문자열 단위 연산
+	TCHAR strTemp[MAX_STRING_LEN];
+
+	for (int i = 0; i < nStr; i++)
+	{
+		for (int j = nStr - 1; j > i; j--)
+		{
+			if (_tcscmp(stringArr[j - 1], stringArr[j]) > 0)
+			{
+				_tcscpy(strTemp, stringArr[j - 1]);
+				_tcscpy(stringArr[j - 1], stringArr[j]);
+				_tcscpy(stringArr[j], strTemp);
+			}
+		}
+	}
+
+	for (int i = 0; i < nStr; i++)
+	{
+		_fputts(stringArr[i], stdout);
+	}
+}
+
+//==========================================================================
 // 함수 TCHAR int CmdProcessing(int)
 // 기능 : 명령어를 입력 받아서 해당 명렁어에 지정되어 있는 기능을 수행한다.
 //			exit가 입력되면 TRUE를 반환하고 이는 프로그램의 종료로 이어진다.
@@ -145,6 +294,48 @@ int CmdProcessing(int tokenNum)
 		}
 
 		_tprintf(_T("echo Message : %s \n"), optString);
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("lp")))
+	{
+		ListProcessInfo();
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("kp")))
+	{
+		if (tokenNum < 2)
+		{
+			_tprintf(_T("usage : kp <process Name> \n"));
+			return 0;
+		}
+
+		KillProcess();
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("kp")))
+	{
+		if (!_tcscmp(cmdTokenList[1], _T(">")))
+		{
+			SECURITY_ATTRIBUTES fileSec = {
+				sizeof(SECURITY_ATTRIBUTES), NULL, TRUE
+			};
+
+			// 상속이 가능해야 지정 가능하다
+			HANDLE hFile = CreateFile(
+				cmdTokenList[2], GENERIC_WRITE, FILE_SHARE_READ,
+				&fileSec, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+			);
+
+			si.hStdOutput = hFile;
+			si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+			si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+			si.dwFlags |= STARTF_USESTDHANDLES;
+
+			isRun = CreateProcess(
+				NULL, cmdTokenList[0], NULL, NULL,
+				TRUE, 0, NULL, NULL, &si, &pi
+			);
+
+			WaitForSingleObject(pi.hProcess, INFINITE);
+			CloseHandle(hFile);
+		}
 	}
 	else
 	{
